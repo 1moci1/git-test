8d2405b5cd8c0f5ca40f790ac7ae3ee78537cf93
Moci <Moci@gmail.com>
2022-04-04 00:08:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Dail
+ * @Date 2022-04-04 0:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
