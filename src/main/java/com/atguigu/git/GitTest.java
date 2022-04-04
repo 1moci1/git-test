947536e64d178328599f5d5893c427ffed3444a9
Moci <Moci@gmail.com>
2022-04-04 09:50:28 +0800

    hot-fix commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
+        System.out.println("hello git111!");
+        System.out.println("hello git222!");
+
+
     }
 }
