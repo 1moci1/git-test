70c48ae7d7e9782831a266b97d36442153187c21
Moci <Moci@gmail.com>
2022-04-04 11:37:44 +0800

    gitee push

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git111!");
         System.out.println("hello push test");
+        System.out.println("hello gitee push test");
     }
 }
