4c2a4a6c17505f2e16cde12c09df212f2cdde4e6
Moci <Moci@gmail.com>
2022-04-04 10:00:14 +0800

    fourth commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git111!");
         System.out.println("hello git222!");
+        System.out.println("hello master git222!");
     }
 }
